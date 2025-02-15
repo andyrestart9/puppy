e27b5e7bba380c798aa35a642c38b32b233d708e
Andy Lin <andylin@Andys-MacBook-Pro.local>
2025-02-15 17:01:05 +0800

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/andyrestart9/puppy
 
 go 1.23.6
+
+require github.com/andyrestart9/dog v0.0.0-20250215084519-3067746a3e23 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/andyrestart9/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BidBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BidBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
